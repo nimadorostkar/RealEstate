0915613882cf0b9380c202bf2332a333bf97c7e8
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-20 03:24:37 +0430

    add urls tp app

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+from app import views
+
+app_name='app'
+
+
+
+
+urlpatterns = [
+    # Matches any html file
+    re_path(r'^.*\.html', views.pages, name='pages'),
+    # The home page
+    path('', views.index, name='home'),
+]
--- core/settings.py
+++ core/settings.py
@@ -99,9 +99,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
