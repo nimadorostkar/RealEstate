45ab5f9a48d6e8648d696f9a85d322822c9e0b85
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-20 03:25:23 +0430

    push

--- app/urls.py
+++ app/urls.py
@@ -7,8 +7,5 @@ app_name='app'
 
 
 urlpatterns = [
-    # Matches any html file
-    re_path(r'^.*\.html', views.pages, name='pages'),
-    # The home page
     path('', views.index, name='home'),
 ]
