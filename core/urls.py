0429abcf1ed8747f3fbde20d281de1a1656e9eb2
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-20 03:15:27 +0430

    push urls

--- core/urls.py
+++ core/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # add this
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
+    #path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path("", include("app.urls"))  # add this
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
