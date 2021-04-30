1070cee80cd8db7da611f3431307dcf41c0f6602
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-30 23:06:32 +0430

    add static files...

--- core/settings.py
+++ core/settings.py
@@ -175,11 +175,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/collectstatic')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/STATICFILES'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # static files are here !!
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
