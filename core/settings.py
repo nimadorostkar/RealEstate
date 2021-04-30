e7aee79dc1211859791789451bd2f66e66916a8a
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-30 22:00:46 +0430

    push4

--- core/settings.py
+++ core/settings.py
@@ -177,8 +177,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/STATICFILES'),)
+
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
 
