bdf8760e55965fe2edd3270abf949585ef2f74a6
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-30 21:54:23 +0430

    add imports to views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from . import models
+from django.contrib.auth.models import User
+#from .models import Profile, Ticket, Order
+#from .forms import ProfileForm, UserForm, TicketForm
+
+
+
+
+
+
+# ----------------------------------------------------- index -----------------
+
+@login_required()
+def index(request):
+
+    context = {}
+    context['segment'] = 'index'
+
+    html_template = loader.get_template( 'index.html' )
+    return HttpResponse(html_template.render(context, request))
