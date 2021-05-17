d2ba1ed858ddd4e4dfa9acc8a2d24894841bca97
nimadorostkar <nimadorostkar97@gmail.com>
2021-05-17 16:16:31 +0430

    ItemAdmin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models
+from django.contrib.admin.models import LogEntry
+from .models import Profile
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin, ImportExportMixin
 
-# Register your models here.
+
+
+
+admin.site.site_header= "tttttt"
+admin.site.site_title= "ttttt"
+
+admin.site.register(LogEntry)
+
+
+
+class ItemAdmin(ImportExportModelAdmin):
+    list_display = ('name','short_description','image_tag')
+    #list_filter = ("manager", "position")
+
+admin.site.register(models.Item, ItemAdmin)
