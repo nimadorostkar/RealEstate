0f8935511aa8e66d2320a39dcbd521ba69943a4b
nimadorostkar <nimadorostkar97@gmail.com>
2021-05-17 15:58:26 +0430

    add extra fields to Item

--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.html import format_html
+from django.urls import reverse
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+import uuid
+from django.template.defaultfilters import truncatechars
 
-# Create your models here.
 
 
 
@@ -8,39 +14,46 @@ from django.db import models
 #------------------------------------------------------------------------------
 class Item(models.Model):
 
-        CHOICES1 = ( ('R','Rent'), ('B','Buy'), ('M','Mortgage') )
-        CHOICES2 = ( ('R','Residential'), ('C','Commercial') )
+     CHOICES1 = ( ('R','Rent'), ('B','Buy'), ('M','Mortgage') )
+     CHOICES2 = ( ('R','Residential'), ('C','Commercial') )
+     CHOICES3 = ( ('V','Villa'), ('A','Apartment') )
 
-    name = models.CharField(max_length=400,verbose_name = "نام")
+    #code =
     buy_status = models.CharField(max_length=1,choices=CHOICES1,verbose_name = "وضعیت خرید")
     estate_status = models.CharField(max_length=1,choices=CHOICES2,verbose_name = "وضعیت ملک")
+    building_status = models.CharField(max_length=1,choices=CHOICES3,verbose_name = "وضعیت ساختمان")
     area_size = models.IntegerField(null=True,blank=True, verbose_name = "متراژ")
     roomـqty = models.IntegerField(null=True,blank=True, verbose_name = "تعداد اتاق")
     parking = models.BooleanField(default=True, verbose_name = "پارکینگ" )
     storage_room = models.BooleanField(default=True, verbose_name = "انباری" )
-
-    description=models.TextField(max_length=1000,null=True, blank=True,verbose_name = "مشخصات")
-    min_inventory = models.IntegerField(null=True,blank=True, verbose_name = " حداقل موجودی ")
-    manager = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True,verbose_name = "مسئول")
-    supplier = models.CharField(max_length=300,null=True, blank=True,verbose_name = "تامین کننده")
-    pro_cap_day = models.IntegerField(default='1', null=True,blank=True, verbose_name = " ظرفیت تولید در روز ")
-    percent_error = models.IntegerField(default='1', null=True,blank=True, verbose_name = " درصد خطا ")
+    building_age = models.IntegerField(null=True,blank=True, verbose_name = "سن بنا")
+    elevator = models.BooleanField(default=False, verbose_name = "آسانسور" )
+    balcony = models.BooleanField(default=False, verbose_name = "بالکن" )
+    image = models.ImageField(upload_to='media', default='media/Default.png' ,null=True, blank=True,verbose_name = "تصویر")
+    additional_Information = models.TextField(max_length=1000,null=True, blank=True,verbose_name = "اطلاعات تکمیلی")
+    city = models.CharField(max_length=50,null=True, blank=True,verbose_name = "شهر")
+    neighbourhood = models.CharField(max_length=50,null=True, blank=True,verbose_name = "محله")
     #location = LocationField(null=True,blank=True)
 
-    class MPTTMeta:
-        level_attr = 'mptt_level'
-        order_insertion_by=['name']
 
     class Meta:
-        verbose_name = "فرآیند"
-        verbose_name_plural = " فرآیند ها "
+        verbose_name = "ملک"
+        verbose_name_plural = "املاک"
 
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('app:processes_detail',args=[self.id])
+    #def get_absolute_url(self):
+        #return reverse('app:item_detail',args=[self.id])
 
     @property
     def short_description(self):
-        return truncatechars(self.description, 70)
+        return truncatechars(self.additional_Information, 70)
+
+
+
+
+
+
+
+# End
